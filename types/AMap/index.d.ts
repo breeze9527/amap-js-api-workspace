5f3d454828607a0140b3202bb0b1953c41123a4f
breeze9527 <breeze9527@gmail.com>
2018-12-05 18:20:27 +0800

    Rename: test => example Update tsconfig.files

--- types/AMap/index.d.ts
+++ types/AMap/index.d.ts
@@ -1,2 +1,7 @@
-declare namespace AMap {
-}
+/// <reference path="Bounds.d.ts" />
+/// <reference path="Event.d.ts" />
+/// <reference path="Layer.d.ts" />
+/// <reference path="LngLat.d.ts" />
+/// <reference path="Map.d.ts" />
+/// <reference path="Pixel.d.ts" />
+/// <reference path="Size.d.ts" />
