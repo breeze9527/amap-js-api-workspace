d13a0f0a99dababe4ad6f170be2fcd873df7b71d
breeze9527 <breeze9527@gmail.com>
2019-03-25 19:47:48 +0800

    Js-doc of advanced-info-window

--- types/advanced-info-window/index.d.ts
+++ types/advanced-info-window/index.d.ts
@@ -13,19 +13,50 @@ declare namespace AMap {
             error: Event<'error'>; // TODO
         }
         interface Options<ExtraData = any> extends InfoWindow.Options<ExtraData> {
+            /**
+             * 结果列表的HTML容器id或容器元素
+             */
             panel?: string | HTMLElement;
+            /**
+             * 设定周边搜索的半径，单位：米
+             */
             searchRadius?: number;
+            /**
+             * 是否支持显示周边搜索
+             */
             placeSearch?: boolean;
+            /**
+             * 是否支持驾车路径规划
+             */
             driving?: boolean;
+            /**
+             * 是否支持步行路径规划
+             */
             walking?: boolean;
+            /**
+             * 是否支持公交路径规划
+             */
             transit?: boolean;
+            /**
+             * 是否支持作为路径规划的起点
+             */
             asOrigin?: boolean;
+            /**
+             * 是否支持作为路径规划的终点
+             */
             asDestination?: boolean;
         }
     }
 
     class AdvancedInfoWindow<ExtraData = any> extends InfoWindow<ExtraData> {
+        /**
+         * 高级信息窗体
+         * @param options 选项
+         */
         constructor(options?: AdvancedInfoWindow.Options);
+        /**
+         * 清除信息窗体在地图上绘制的路线规划或者搜索的结果
+         */
         clear(): void;
     }
 }
