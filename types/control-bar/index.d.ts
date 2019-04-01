5ae0958c6c8d4e370ce5802cbfc2ff3f9cbac371
breeze9527 <breeze9527@gmail.com>
2019-04-01 23:01:32 +0800

    Update type: control-bar

--- types/control-bar/index.d.ts
+++ types/control-bar/index.d.ts
@@ -15,12 +15,25 @@ declare namespace AMap {
             left?: string;
         }
         interface Options {
+            /**
+             * 显示位置
+             */
             position?: Position;
+            /**
+             * 是否显示缩放按钮
+             */
             showZoomBar?: boolean;
+            /**
+             * 是否显示倾斜、旋转按钮
+             */
             showControlButton?: boolean;
         }
     }
     class ControlBar extends EventEmitter {
+        /**
+         * 组合了旋转、倾斜、复位、缩放在内的地图控件
+         * @param options 选项
+         */
         constructor(options?: ControlBar.Options);
         // internal
         show(): void;
